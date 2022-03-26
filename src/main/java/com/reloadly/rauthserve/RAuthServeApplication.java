5630ce499a10d3face15334e39672a374569ca4a
lewis.ugwu <lewis.ugwu@teamapt.com>
2022-03-26 15:04:34 +0100

    initial commit

---
+++ src/main/java/com/reloadly/rauthserve/RAuthServeApplication.java
@@ -0,0 +1,13 @@
+package com.reloadly.rauthserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RAuthServeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RAuthServeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reloadly/rauthserve/RAuthServeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reloadly.rauthserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RAuthServeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
